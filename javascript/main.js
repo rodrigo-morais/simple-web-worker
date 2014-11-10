a4466991ef7873f86d38c82a7fe07b259ed20bfc
Rodrigo Morais <morais.rm@gmail.com>
2014-11-10 13:01:00 -0200

    Creating repo.

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+﻿if (window.Worker) {
+    var worker = new Worker('javascript/worker.js');
+
+    worker.addEventListener('message', function (e) {
+        var repoList = document.querySelector(".repos"),
+            repos = e.data;
+
+        repos.forEach(function (repo) {
+            console.log(repo);
+            var repoElement = document.createElement('li'),
+                linkRepo = document.createElement('a');
+
+            linkRepo.appendChild(document.createTextNode(repo.name));
+            linkRepo.href = repo.html_url;
+            linkRepo.target = '_blank';
+
+            repoElement.appendChild(linkRepo);
+            repoElement.appendChild(document.createTextNode(' - ' + repo.description));
+
+            repoList.appendChild(repoElement);
+        });
+
+    }, false);
+
+    worker.postMessage('');
+}
---
+++ javascript/worker.js
@@ -0,0 +1,28 @@
+﻿importScripts('jquery.hive.pollen.js');
+
+$(function (data) {
+
+    var page = 1,
+        per_page = 3,
+        time = 2000;
+
+    setInterval(function () {
+        var url = 'https://api.github.com/users/rodrigo-morais/repos?page=' + page + '&per_page=' + per_page;
+
+        $.ajax.get({
+            url: url,
+            dataType: 'json',
+            success: function (repos) {
+
+                self.postMessage(repos);
+
+                if (repos.length === 0) {
+                    self.close();
+                }
+            }
+        });
+
+        page = page + 1;
+
+    }, time);
+});
